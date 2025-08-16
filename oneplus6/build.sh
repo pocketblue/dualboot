fa4ec4c81d52f77c4e001d4d97d8a92308df0199
gmanka <gmankab@gmail.com>
2025-08-17 03:10:04 +0500

    [u] switch to twrp

--- oneplus6/build.sh
+++ oneplus6/build.sh
@@ -1,14 +1,11 @@
 mkdir images
 
-curl -L https://dl.orangefox.download/635928283c05f43c193c39e2 -o ofox.zip
-7z x ofox.zip -o./ofox
-mv ofox/recovery.img images/ofox_enchilada.img
-
 curl -L https://github.com/fedora-remix-mobility/u-boot/releases/download/fedora-mobility-v0.0.1/uboot-sdm845-oneplus-enchilada.img -o images/uboot_enchilada.img
 curl -L https://mirrorbits.lineageos.org/full/enchilada/20250813/boot.img -o images/lineage_boot.img
 curl -L https://mirrorbits.lineageos.org/full/enchilada/20250813/dtbo.img -o images/lineage_dtbo.img
 curl -L https://github.com/gmankab/sgdisk/releases/download/v1.0.10/sgdisk -o images/sgdisk
 curl -L https://github.com/gmankab/parted/releases/download/v3.6/parted -o images/parted
+curl -L -e https://dl.twrp.me/enchilada https://dl.twrp.me/enchilada/twrp-3.7.0_11-0-enchilada.img -o images/twrp_enchilada.img
 
 curl -L https://mirrorbits.lineageos.org/full/enchilada/20250813/lineage-22.2-20250813-nightly-enchilada-signed.zip -o images/lineage_rom.zip
 
