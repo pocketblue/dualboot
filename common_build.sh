0376717c6c030c332d0ce6adac75d086573c1e02
gmanka <gmankab@gmail.com>
2025-09-21 03:17:15 +0500

    [u] use gz

--- common_build.sh
+++ common_build.sh
@@ -1,7 +1,7 @@
 mv images/boot.raw images/fedora_boot.raw
 mv images/esp.raw  images/fedora_esp.raw
 mv images/root.raw  images/fedora_root.raw
-7z a -txz -mx=9 images/fedora_root.raw.xz images/fedora_root.raw
+7z a -mx=9 images/fedora_root.raw.gz images/fedora_root.raw
 rm images/fedora_root.raw
 curl -L https://github.com/gmankab/sgdisk/releases/download/v1.0.10/sgdisk -o images/sgdisk
 curl -L https://github.com/gmankab/parted/releases/download/v3.6/parted -o images/parted
