9e9ec60a986150ee59c0732ff62f030b1ad12e17
gmanka <gmankab@gmail.com>
2025-09-22 12:01:17 +0500

    [u] change path to fedora root

--- common_build.sh
+++ common_build.sh
@@ -1,6 +1,11 @@
 mv images/boot.raw images/fedora_boot.raw
 mv images/esp.raw  images/fedora_esp.raw
 mv images/root.raw  images/fedora_root.raw
+
+sudo mount images/fedora_boot.raw /mnt
+sudo sed -i '/^options / s#ostree=/ostree/#ostree=/.stowaways/fedora/ostree/#' /mnt/loader/entries/ostree-1.conf
+sudo umount /mnt
+
 7z a -mx=9 images/fedora_root.raw.gz images/fedora_root.raw
 rm images/fedora_root.raw
 curl -L https://github.com/gmankab/sgdisk/releases/download/v1.0.10/sgdisk -o images/sgdisk
