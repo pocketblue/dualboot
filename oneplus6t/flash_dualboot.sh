7f516a043fcfa1c358950c77fa3f68260e42d0e3
gmanka <gmankab@gmail.com>
2025-09-22 12:01:54 +0500

    [u] flashing scripts

--- oneplus6t/flash_dualboot.sh
+++ oneplus6t/flash_dualboot.sh
@@ -9,9 +9,8 @@ echo 'waiting for device appear in fastboot'
 fastboot getvar product 2>&1 | grep sdm845
 fastboot erase op2 2> /dev/null || true
 fastboot flash dtbo_a images/lineage_dtbo.img
-fastboot flash dtbo_b images/lineage_dtbo.img
+fastboot flash boot_a images/twrp_fajita.img
 fastboot --set-active=a
-fastboot flash boot images/twrp_enchilada.img --slot=all
 fastboot reboot
 
 adb wait-for-recovery
@@ -32,27 +31,37 @@ adb shell parted -s /dev/block/sda -- mkpart userdata    ext4 $start -3GB
 adb shell parted -s /dev/block/sda -- mkpart fedora_boot ext4   -3GB -1GB
 adb shell parted -s /dev/block/sda -- mkpart fedora_esp  fat32  -1GB 100%
 
-# installing lineage os rom
+# installing lineage os rom on slot b
 adb push images/lineage_rom.zip /tmp/lineage_rom.zip
 adb shell twrp format data
-echo 'installing lineage, you can check progress on screen of your device'
+echo 'installing lineage os rom on slot b'
+echo 'you can check progress on screen of your device'
 adb shell twrp install /tmp/lineage_rom.zip
 adb shell rm /tmp/lineage_rom.zip
 
+# disable encryption on slot b
+adb shell mount /dev/block/by-name/vendor_b /mnt
+adb shell 'sed -E -i "s/,?fileencryption=[^, ]*//g" /mnt/etc/fstab.qcom'
+adb shell umount /mnt
+adb shell twrp format data
+
 # installing fedora root
-adb push images/fedora_root.tar.xz /tmp/fedora_root.tar.xz
-adb shell rm -rf /data/ostree
-echo 'installing fedora root, this can be long'
-adb shell tar -xJf /tmp/fedora_root.tar.xz -C /data
-echo 'done installing fedora root'
-adb shell rm /tmp/fedora_root.tar.xz
-adb reboot bootloader
+adb shell rm -rf /data/.stowaways/fedora
+adb shell mkdir -p /data/.stowaways/fedora
+adb push images/fedora_root.raw.gz /data/.stowaways/fedora_root.raw.gz
+adb shell gzip -d /data/.stowaways/fedora_root.raw.gz
+adb shell mount -o loop,ro /data/.stowaways/fedora_root.raw /mnt
+adb shell cp -a /mnt/ostree /data/.stowaways/fedora
+adb shell mkdir -p /data/.stowaways/fedora/boot
+adb shell umount /mnt
+adb shell rm /data/.stowaways/fedora_root.raw
+adb reboot fastboot
 
 echo 'waiting for device appear in fastboot'
 fastboot getvar product 2>&1 | grep sdm845
 fastboot erase dtbo_a
 fastboot flash dtbo_b      images/lineage_dtbo.img
-fastboot flash boot_a      images/uboot_enchilada.img
+fastboot flash boot_a      images/uboot_fajita.img
 fastboot flash boot_b      images/lineage_boot.img
 fastboot flash fedora_esp  images/fedora_esp.raw
 fastboot flash fedora_boot images/fedora_boot.raw
