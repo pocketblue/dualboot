4797775c7a04fa75281a50c87fd8125406e6b87b
gmanka <gmankab@gmail.com>
2025-09-22 05:48:50 +0500

    [u] lineage os links

--- oneplus6/build.sh
+++ oneplus6/build.sh
@@ -4,9 +4,9 @@ set -uexo pipefail
 
 cp -r ../images ./images
 curl -L https://github.com/fedora-remix-mobility/u-boot/releases/download/fedora-mobility-v0.0.1/uboot-sdm845-oneplus-enchilada.img -o images/uboot_enchilada.img
+curl -L -e https://dl.twrp.me/enchilada https://dl.twrp.me/enchilada/twrp-3.7.0_11-0-enchilada.img -o images/twrp_enchilada.img
 curl -L https://mirrorbits.lineageos.org/full/enchilada/20250917/boot.img -o images/lineage_boot.img
 curl -L https://mirrorbits.lineageos.org/full/enchilada/20250917/dtbo.img -o images/lineage_dtbo.img
-curl -L -e https://dl.twrp.me/enchilada https://dl.twrp.me/enchilada/twrp-3.7.0_11-0-enchilada.img -o images/twrp_enchilada.img
 curl -L https://mirrorbits.lineageos.org/full/enchilada/20250917/lineage-22.2-20250917-nightly-enchilada-signed.zip -o images/lineage_rom.zip
 install -Dm 0755 dualboot.sh dualboot_oneplus6_enchilada.sh
 
