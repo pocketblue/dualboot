685ac3689cc0d9afa994466a0d1ed2e9c24611da
gmanka <gmankab@gmail.com>
2025-09-24 15:20:51 +0500

    [f] twrp for fajita

--- oneplus6/build_device.sh
+++ oneplus6/build_device.sh
@@ -7,7 +7,7 @@ cp -r images $device
 cd $device
 
 curl -L https://github.com/fedora-remix-mobility/u-boot/releases/download/fedora-mobility-v0.0.1/uboot-sdm845-oneplus-$device.img -o images/uboot_$device.img
-curl -L -e https://dl.twrp.me/$device https://dl.twrp.me/$device/twrp-3.7.0_11-0-$device.img -o images/twrp_$device.img
+curl -L -e https://dl.twrp.me/$device https://dl.twrp.me/$device/twrp-$twrp-$device.img -o images/twrp_$device.img
 
 curl -L https://mirrorbits.lineageos.org/full/$device/$lineage_build/boot.img -o images/lineage_boot.img
 curl -L https://mirrorbits.lineageos.org/full/$device/$lineage_build/dtbo.img -o images/lineage_dtbo.img
