339be7922885d05ef10ddfdbbd0db5309dd1d59c
gmanka <gmankab@gmail.com>
2025-09-24 10:07:40 +0500

    [u] boot entry, roots
    [-] not edit boot entry
    [u] ship rootfs as archive with files, instead of archive with .raw image

--- oneplus6/build_common.sh
+++ oneplus6/build_common.sh
@@ -2,16 +2,11 @@
 
 set -uexo pipefail
 
-mv images/root.raw images/fedora_root.raw
+sudo mount -o loop,ro images/root.raw /mnt
+sudo tar -C /mnt -czf fedora_root.tar.gz ostree
+
 mv images/boot.raw images/fedora_boot.raw
 mv images/esp.raw  images/fedora_esp.raw
-
-sudo mount images/fedora_boot.raw /mnt
-sudo sed -i '/^options / s#ostree=/ostree/#ostree=/.stowaways/fedora/ostree/#' /mnt/loader/entries/ostree-1.conf
-sudo umount /mnt
-
-7z a -mx=9 images/fedora_root.raw.gz images/fedora_root.raw
-rm images/fedora_root.raw
 
 curl -L https://github.com/gmankab/sgdisk/releases/download/v1.0.10/sgdisk -o images/sgdisk
 curl -L https://github.com/gmankab/parted/releases/download/v3.6/parted -o images/parted
